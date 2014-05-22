09138d68d63b7d5c7669c1085ca039e76ce99d15
John Daily <git@epep.us>
2014-05-22 08:42:52 -0400

    Improve readability of comments, try not to traverse strings

--- cfgdiff.erl
+++ cfgdiff.erl
@@ -35,44 +35,54 @@ diff({Name1, List1}, {Name2, List2}) ->
 %% treediff will only work as designed if the lists are sorted all the
 %% way down
 
-%% Option 1: the property lists match exactly, nothing to do. Also
-%% matches empty lists.
+%% The property lists match exactly, nothing to do. Also matches empty
+%% lists to terminate the recursion.
 treediff({Name1, P1}, {Name2, P2}, Label, Accum) when P1 =:= P2 ->
     lists:reverse(Accum);
-%% Option: ran out of items in only one list
+
+%% Ran out of items in the second list
 treediff({Name1, P1}, {Name2, []}, Label, Accum) ->
     [{Label, {Name1, P1}}] ++ Accum;
-%% Option: ran out of items in only one list
-treediff([], {Name2, P2}, Label, Accum) ->
+
+%% Ran out of items in the first list
+treediff({Name1, []}, {Name2, P2}, Label, Accum) ->
     [{Label, {Name2, P2}}] ++ Accum;
-%% Option 2: the head of each list matches, check the tails
-treediff({Name1, [H1|T1]=P1}, {Name2, [H1|T2]=P2}, Label, Accum) ->
+
+%% The head of each list matches exactly, check the tails. Match the
+%% full tuple at the head of each list so that we don't accidentally
+%% start matching integers in strings
+treediff({Name1, [{_K1, _V1}|T1]=P1}, {Name2, [{_K1, _V1}|T2]=P2}, Label, Accum) ->
     treediff({Name1, T1}, {Name2, T2}, Label, Accum);
-%% Option 3: the key at the head of each list matches, check
-%% the nested values before checking the tails
+
+%% The key at the head of each list matches, check the nested values
+%% before checking the tails
 treediff({Name1, [{K1, V1}|T1]=P1}, {Name2, [{K1, V2}|T2]=P2}, Label, Accum) ->
     treediff({Name1, T1}, {Name2, T2}, Label,
              treediff({Name1, V1}, {Name2, V2},
                       Label ++ "/" ++ atom_to_list(K1), Accum));
-%% Option 4: the key at the head of list 1 does not exist in list
-%% 2. Capture the head of list 1 and try again with the next element,
-%% but retain the entirety of list 2
+
+%% The key at the head of list 1 does not exist in list 2. Capture the
+%% head of list 1 and try again with the next element; continue with the
+%% entirety of list 2
 treediff({Name1, [{K1, V1}|T1]=P1}, {Name2, [{K2, V2}|T2]=P2}, Label, Accum)
   when K1 < K2 ->
     treediff({Name1, T1}, {Name2, P2}, Label,
              [{Label, {Name1, {K1, V1}}}] ++ Accum);
-%% Option 5: the key at the head of list 2 does not exist in list
-%% 1. Capture the head of list 2 and try again with the next element,
-%% but retain the entirety of list 1.
+
+%% The key at the head of list 2 does not exist in list 1. Capture the
+%% head of list 2 and try again with the next element; continue with
+%% the entirety of list 1
 %%
 %% Guard is redundant but makes things more explicit.
 treediff({Name1, [{K1, V1}|T1]=P1}, {Name2, [{K2, V2}|T2]=P2}, Label, Accum)
   when K1 > K2 ->
     treediff({Name1, P1}, {Name2, T2}, Label,
              [{Label, {Name2, {K2, V2}}}] ++ Accum);
+
+%% If we reach this point, one or the other isn't actually a proplist,
+%% and they don't match, so let's just capture the different values
+%% and move on
 treediff({Name1, P1}, {Name2, P2}, Label, Accum) ->
-    %% If we reach this point, one or the other isn't actually a list,
-    %% so let's just capture the different values and move on
     [{Label, {Name1, P1}, {Name2, P2}}] ++ Accum.
 
 
